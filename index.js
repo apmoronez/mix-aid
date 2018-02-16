73e9f8b1a5554fbfc11c5ff23a35517c7d0f6afe
Gomez <apmoronez@gmail.com>
2018-02-16 15:33:31 -0500

    adding basic UI for selecting card attributes, showing card images

--- index.js
+++ index.js
@@ -96,40 +96,192 @@ var deleteparser = new CSVParse({columns: true}, function(err, output) {
     }
 });
 
-/*
-var input = Fs.createReadStream(__dirname + '/cardlist.csv');
-input.pipe(parser);
-console.log('loaded data');
-*/
+var getUI = function(req) {
+    var fields = {
+	Playlist: [
+	    {
+		playlist: [
+                    { label: 'Sweets', value: 'Sweets'},
+                    { label: 'Highness', value: 'Highness'},
+                    { label: 'Blade', value: 'Blade'},
+                    { label: 'Controller', value: 'Controller'},
+                    { label: 'Derby', value: 'Derby'},
+                    { label: 'Mirrors', value: 'Mirrors'},
+                    { label: 'Ouroboros', value: 'Ouroboros'},
+                    { label: 'Astro', value: 'Astro'},
+                    { label: 'Lucky',value: 'Lucky'},
+                    { label: 'Flawless',value: 'Flawless'},
+                    { label: 'Bomb',value: 'Bomb'},
+		],
+	    },
+	    {
+		playlist: [
+                    { label: 'Chiller',value: 'Chiller'},
+                    { label: 'Dapper',value: 'Dapper'},
+                    { label: 'Seer',value: 'Seer'},
+                    { label: 'Hightower',value: 'Hightower'},
+                    { label: 'Puff',value: 'Puff'},
+                    { label: 'Fever',value: 'Fever'},
+                    { label: 'Breaker',value: 'Breaker'},
+                    { label: 'Socket',value: 'Socket'},
+                    { label: 'Moonlight',value: 'Moonlight'},
+                    { label: 'Baffler',value: 'Baffler'},
+                    { label: 'DM(Promo)',value: 'DM'},
+		],
+	    },
+	],
+	Level: [
+	    {
+		level: [
+		    { label: '1', value: 1},
+		    { label: '2', value: 2},
+		    { label: '3', value: 3},
+		],
+	    },
+	],
+	Color: [
+	    {
+		isYellow: [
+                    { label: 'Yellow', value: 'true'},
+		],
+		isRed: [
+                    { label: 'Red', value: 'true'},
+		],
+		isBlue: [
+                    { label: 'Blue', value: 'true'},
+		],
+		isGreen: [
+                    { label: 'Green', value: 'true'},
+		],
+		isWild: [
+                    { label: 'Wild', value: 'true'},
+		],
+		isFX: [
+                    { label: 'White/FX', value: 'true'},
+		],
+	    },
+	],
+	Instrument: [
+	    {
+		yellowInstrument: [
+                    { label: 'Yellow Drums', value: 'Drums'},
+                    { label: 'Yellow Guitar', value: 'Guitar'},
+                    { label: 'Yellow Horns', value: 'Horns'},
+                    { label: 'Yellow Keys', value: 'Keys'},
+                    { label: 'Yellow Sampler', value: 'Sampler'},
+                    { label: 'Yellow Strings', value: 'Strings'},
+                    { label: 'Yellow Vocals', value: 'Vocals'},
+		],
+	    },
+	    {
+		redInstrument: [
+                    { label: 'Red Drums', value: 'Drums'},
+                    { label: 'Red Guitar', value: 'Guitar'},
+                    { label: 'Red Horns', value: 'Horns'},
+                    { label: 'Red Keys', value: 'Keys'},
+                    { label: 'Red Sampler', value: 'Sampler'},
+                    { label: 'Red Strings', value: 'Strings'},
+                    { label: 'Red Vocals', value: 'Vocals'},
+                ],
+	    },
+	    {
+		blueInstrument: [
+                    { label: 'Blue Drums', value: 'Drums'},
+                    { label: 'Blue Guitar', value: 'Guitar'},
+                    { label: 'Blue Horns', value: 'Horns'},
+                    { label: 'Blue Keys', value: 'Keys'},
+                    { label: 'Blue Sampler', value: 'Sampler'},
+                    { label: 'Blue Strings', value: 'Strings'},
+                    { label: 'Blue Vocals', value: 'Vocals'},
+                ],
+	    },
+	    {
+		greenInstrument: [
+                    { label: 'Green Drums', value: 'Drums'},
+                    { label: 'Green Guitar', value: 'Guitar'},
+                    { label: 'Green Horns', value: 'Horns'},
+                    { label: 'Green Keys', value: 'Keys'},
+                    { label: 'Green Sampler', value: 'Sampler'},
+                    { label: 'Green Strings', value: 'Strings'},
+                    { label: 'Green Vocals', value: 'Vocals'},
+                ],
+	    },
+	],
+    };
+	
+    var output = "<h1>Gomez Mix-Aid</h1><br/>";
 
-/*
-redis.getAllMatchingCardsBySetPairwiseMultipleOR([
-    {
-	key: 'yellowInstrument',
-	value: 'Strings',
-    },
-    {
-	key: 'redInstrument',
-	value: 'Strings',
-    },
-    {
-	key: 'blueInstrument',
-	value: 'Strings',
-    },
-    {
-	key: 'greenInstrument',
-	value: 'Strings',
-    },
-], function(err, res) {
-    if (err) {
-	console.log("Error getting card: " + err);
+    output += "<form method=GET>Find cards that match <select name=connective>";
+    var andSelected = "";
+    var orSelected = "";
+    if (req && req.query && req.query.connective) {
+	if (req.query.connective == 'AND') {
+	    andSelected = "selected";
+	}
+	if (req.query.connective == 'OR') {
+	    orSelected = "selected";
+	}
     }
-    else {
-	console.log("Got cards!");
-	console.dir(res);
+
+    output += "<option value=AND " + andSelected + ">ALL</option><option value=OR " + orSelected + ">ANY</option>";
+    output += "</select> of the following attributes:<br\><br\><table border=0 cellpadding=10 cellspacing=0><tr>";
+
+    var sectionList = Object.keys(fields);
+    for (var sectionIndex=0; sectionIndex < sectionList.length; sectionIndex++) {
+	output += "<td valign=top><h2>" + sectionList[sectionIndex] + "</h2><table border=0 cellspacing=0 cellpadding=0><tr>";
+	var groupList = fields[sectionList[sectionIndex]];
+	for (var groupIndex=0; groupIndex < groupList.length; groupIndex++) {
+	    output += "<td>";
+	    var fieldList = Object.keys(groupList[groupIndex]);
+	    for (var fieldListIndex=0; fieldListIndex < fieldList.length; fieldListIndex++) {
+		for (var i=0; i < groupList[groupIndex][fieldList[fieldListIndex]].length; i++) {
+		    var fieldData = groupList[groupIndex][fieldList[fieldListIndex]][i];
+		    var checked = "";
+		    if (req && req.query && req.query[fieldList[fieldListIndex]]) {
+			var values = req.query[fieldList[fieldListIndex]];
+			if (typeof(values) == 'string') {
+			    if (values == fieldData.value) {
+				checked = "checked";
+			    }
+			}
+			else {
+			    for (var j=0; j < values.length; j++) {
+				if (values[j] == fieldData.value) {
+				    checked = "checked";
+				    break;
+				}
+			    }
+			}
+		    }
+		    output += "<input type=checkbox name=" + fieldList[fieldListIndex] + " value=" + fieldData.value + " " + checked + " />" + fieldData.label + "<br/>";
+		}
+	    }
+	    output += "</td>";
+	}
+	output += "</tr></table></td>";
     }
-});
-*/
+    output += "</tr></table>";
+    output += "<br/><input type=submit value=Go /><input type=checkbox name=jsononly value=true />JSON response only</form>";
+    output += "<br/><br/>Example queries:<br/>\
+<a href='/?playlist=Mirrors'>/?playlist=Mirrors</a><br/>\
+<a href='/?isGreen=true&level=2'>/?isGreen=true&level=2</a><br/>\
+<a href='/?greenInstrument=Horns&yellowInstrument=Horns&redInstrument=Horns&connective=OR'>/?greenInstrument=Horns&yellowInstrument=Horns&redInstrument=Horns&connective=OR</a><br/>";
+    return output;
+};
+
+var getCardImageHTML = function(data) {
+    var outputHTML = "";
+    for (var i=0; i < data.length; i++) {
+	var img = "<img src=https://sad.hasbro.com/1200e194df42fdb4868a9811c626c142ad91b760/" + data[i].cardHash + ".png height=260 width=190 alt=\"" + data[i].artist + " - " + data[i].song + " (" + data[i].playlist + " " + data[i].id + ")\" />";
+	outputHTML += img;
+    }
+    return outputHTML;
+};
+
+var cardSort = function(a,b) {
+    return (a.id < b.id) ? -1 : ((a.id > b.id) ? 1 : 0);
+};
+
 App.get('/load-data', function(req, res) {
     var input = Fs.createReadStream(__dirname + '/cardlist.csv');
     input.pipe(loadparser);
@@ -146,28 +298,29 @@ App.get('/', function(req, res) {
     var query = req.query;
     var keys = Object.keys(query);
     if (!keys.length) {
-	var output = "Valid fields<br>";
-	var dataFields = redis.getDataFields();
-	for (var i=0; i < dataFields.length; i++) {
-	    if (dataFields[i].indexed) {
-		output = output + dataFields[i].name + ": " + dataFields[i].type + "<br>";
-	    }
-	}
-	output += "connective: [AND|OR] (defaults to AND)<br>";
-	output += "<br><br>Example queries:<br>\
-<a href='/?playlist=Mirrors'>/?playlist=Mirrors</a><br>\
-<a href='/?isGreen=true&level=2'>/?isGreen=true&level=2</a><br>\
-<a href='/?greenInstrument=Horns&yellowInstrument=Horns&redInstrument=Horns&connective=OR'>/?greenInstrument=Horns&yellowInstrument=Horns&redInstrument=Horns&connective=OR</a><br>";
-	return res.send(output);
+	return res.send(getUI());
     }
     var searchParams = [];
     for (var i=0; i < keys.length; i++) {
-	searchParams.push({
-	    key: keys[i],
-	    value: query[keys[i]],
-	});
+	if (keys[i] == 'connective') {
+	    continue;
+	}
+	var values = query[keys[i]];
+	if (typeof(values) == 'string') {
+	    searchParams.push({
+		key: keys[i],
+		value: values,
+	    });
+	}
+	else {
+	    for (var j=0; j < values.length; j++) {
+		searchParams.push({
+		    key: keys[i],
+		    value: values[j],
+		});
+	    }
+	}
     }
-    
     if (query.connective == 'OR') {
 	redis.getAllMatchingCardsBySetPairwiseMultipleOR(searchParams, function(err, data) {
 	    if (err) {
@@ -175,7 +328,14 @@ App.get('/', function(req, res) {
 		res.status(500).send("Error getting card data!");
 	    }
 	    else {
-		res.json(data);
+		data.sort(cardSort);
+		if (query.jsononly) {
+		    res.json(data);
+		}
+		else {
+		    var output = getCardImageHTML(data);
+		    res.send(getUI(req) + output + "<br/>JSON:<br/><pre>" + JSON.stringify(data) + "</pre>");
+		}
 	    }
 	});
     }
@@ -186,7 +346,14 @@ App.get('/', function(req, res) {
 		res.status(500).send("Error getting card data!");
 	    }
 	    else {
-		res.json(data);
+		data.sort(cardSort);
+		if (query.jsononly) {
+		    res.json(data);
+		}
+		else {
+		    var output = getCardImageHTML(data);
+		    res.send(getUI(req) + output + "<br/>JSON:<br/><pre>" + JSON.stringify(data) + "</pre>");
+		}
 	    }
 	});
     }
